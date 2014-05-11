7c3bd9c4361a768df0c4422757aaec2f98c3f89e
Sergey Zaretsky <zaretskysa@gmail.com>
2014-05-12 00:20:10 +0400

    Use cont monad transformer

--- lab/Cont.hs
+++ lab/Cont.hs
@@ -1,6 +1,8 @@
 module Cont where
 
+import Control.Monad.Identity
 import Control.Monad.Cont
+import Control.Monad.State
 
 data Statement = 
       Expr Int
@@ -13,25 +15,30 @@ data Value =
     | Value Int
     deriving (Show, Eq)
 
-evalStmt :: Statement -> Cont Value Value
+type Eval a = ContT a Identity a
+
+runEval :: Eval Value -> Value
+runEval eval = runIdentity (runContT eval (return . id))
+
+evalStmt :: Statement -> Eval Value
 evalStmt (Expr val) = return $ Value val
 evalStmt (Block stmts) = evalBlock stmts
 evalStmt (Return val) = returnValue $ Value val
 
-returnValue :: Value -> Cont Value Value
+returnValue :: Value -> Eval Value
 returnValue val = cont $ \_ -> val
 
-evalBlock :: [Statement] -> Cont Value Value
+evalBlock :: [Statement] -> Eval Value
 evalBlock [] = return Undefined
 evalBlock [st] = evalStmt st
 evalBlock (st:rest) = evalStmtAndContinue st rest
 
-evalStmtAndContinue :: Statement -> [Statement] -> Cont Value Value
+evalStmtAndContinue :: Statement -> [Statement] -> Eval Value
 evalStmtAndContinue st rest =
     cont $ \next -> runCont (evalStmt st) (\_ -> runCont (evalBlock rest) next)
 
 evalProgram :: [Statement] -> Value
-evalProgram stmts = runCont (evalBlock stmts) id
+evalProgram stmts = runEval $ evalBlock stmts
 
 prog1 = [Expr 1, Block [Return 3, Expr 2], Expr 4] 
 evalProg1 = evalProgram prog1 -- result will be Value 3
